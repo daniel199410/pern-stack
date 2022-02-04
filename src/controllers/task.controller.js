7239991b989fd16a4e18cefa6627a409858b5801
Daniel Cataño Restrepo <dcatanor@unal.edu.co>
2022-02-04 13:13:40 -0500

    Getting a single task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,27 +1,51 @@
 const pool = require('../db');
 
-const getTasks = async (req, res) => {
+const makeRequest = f => {
 	try {
-		const tasks = await pool.query('SELECT * FROM task');
-        	res.json(tasks.rows);
-	}catch(error) {
-                res.status(500);
-                res.json({error: "Ha ocurrido un error"});
-        }
-};
-
-const getTask = async (req, res) => res.send('Retrieving a task');
-
-const createTask = async (req, res) => {
-	try {
-		const { title, description } = req.body;
-        	const result = await pool.query('INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *', [title, description]);
-        	console.log(result);
-        	res.json(result.rows[0]);
+		f();
 	} catch(error) {
 		res.status(500);
-		res.json({error: "Ha ocurrido un error"});
+		res.json({error: 'Ha ocurrido un error'});
 	}
+}
+
+const getTasks = async (req, res) => {
+	makeRequest(async () => {
+		try {
+                	const tasks = await pool.query('SELECT * FROM task');
+                	res.json(tasks.rows);
+        	}catch(error) {
+                	res.status(500);
+                	res.json({error: "Ha ocurrido un error"});
+        	}
+	});
+};
+
+const getTask = async (req, res) => {
+	makeRequest(async () => {
+		const { id } = req.params;
+        	const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
+        	if(result.rows.length === 0) {
+                	return res.status(404).json({message: 'Task not found'});
+        	}
+        	res.json(result.rows[0]);
+	});
+}
+
+const createTask = async (req, res) => {
+	makeRequest(async () => {
+		try {
+                	const { title, description } = req.body;
+                	const result = await pool.query(
+				'INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *',
+				[title, description]
+			);
+                	res.json(result.rows[0]);
+        	} catch(error) {
+                	res.status(500);
+                	res.json({error: "Ha ocurrido un error"});
+        	}
+	});
 }
 
 const deleteTask = async (req, res) => res.send('Deleting a list of tasks');
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/tasks', getTasks);
 
-router.get('/tasks/10', getTask);
+router.get('/tasks/:id', getTask);
 
 router.post('/tasks', createTask);
 
